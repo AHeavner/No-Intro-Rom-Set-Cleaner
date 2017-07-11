ece8c3f9d3ea0a99b2cbd9a6e5430eadffc42421
bpdev97 <bmp0015@bpdev-xps.localdomain>
2017-07-11 15:15:07 -0400

    Fix messages

--- de.py
+++ de.py
@@ -1,9 +1,13 @@
 # The purpose of this script is to delete the foreign versions of roms from No Intro rom sets.
-# To use it, first extract the files from all of the archives in the rom set. Then change the directory
-# variable "dir" to the directory that your extracted roms reside. Open a cmd or terminal in the
-# directory that the script exists in and run "python de.py". This script will delete the archives
-# at the end so do make sure to extract them first.
+# To use it, first extract the files from all of the archives in the rom set. This script 
+# will delete the archives at the end so do make sure to extract them first.
+#
 # This script is meant to keep USA versions only! If you want to keep different versions then change the "patterns" list.
+
+# Authors:
+# @AHeavner
+# @bpdev97
+# Since 7/10/17
 
 import os, re, sys, stat, time
 
@@ -16,9 +20,9 @@ if "-h" in sys.argv:
 	Usage:
 	python de.py [-t ~/RomSet] [-h] [-r] [-l] [-p] 
 	
-	-h	Show the help menu
-	-t	The target directory to be cleaned
-	-m	Renames outputted files to not contain parenthetical data 
+	-h  Show the help menu
+	-t  The target directory to be cleaned
+	-m  Renames outputted files to not contain parenthetical data 
 	-l  Logs data to de.log
 	-p  Performs operations and writes to de.log without modifying original data
 	""")
@@ -59,9 +63,13 @@ def purge(dir, patternObjects):
 		file.write(log)
 		file.close()
 	return deleted
-		
+
 deleted = purge(dir, patternObjects)
 total_time = time.time() - start_time
 
-print("{} files were deleted in {:.2f} seconds.".format(deleted, total_time))
-input("Press enter to exit.")
+if "-p" in sys.argv:
+	print("{} files in {} would be deleted!".format(deleted, dir))
+	input("Press enter to exit.")
+else:
+	print("{} files were deleted in {:.2f} seconds.".format(deleted, total_time))
+	input("Press enter to exit.")
